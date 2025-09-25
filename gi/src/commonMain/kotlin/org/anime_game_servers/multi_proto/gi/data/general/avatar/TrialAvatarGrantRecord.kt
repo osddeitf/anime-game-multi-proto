d36534f44f8ae4ce33b2fc21b8b3f2ad07f20d9b
osddeitf <osddeitf@gmail.com>
2025-09-25 13:19:14 +0700

    update TrialAvatarGrantRecord proto model

--- gi/src/commonMain/kotlin/org/anime_game_servers/multi_proto/gi/data/general/avatar/TrialAvatarGrantRecord.kt
+++ gi/src/commonMain/kotlin/org/anime_game_servers/multi_proto/gi/data/general/avatar/TrialAvatarGrantRecord.kt
@@ -7,6 +7,6 @@ import org.anime_game_servers.core.base.annotations.proto.ProtoModel
 @AddedIn(Version.GI_CB2)
 @ProtoModel
 internal interface TrialAvatarGrantRecord {
-    var grantReason: Int
+    var grantReason: GrantReason
     var fromParentQuestId: Int
 }
